770ed32e6927f6383e3280e2459635a3cd5c37ee
Laziz Rahmat <lazizrakhmat@gmail.com>
2023-01-06 01:40:36 +0500

    first commit, added sql query

---
+++ SQLQuery1.sql
@@ -0,0 +1,18 @@
+--INSERT INTO EmployeeDemograpihcs VALUES
+--(1002, 'Jane', 'Hobby', 30, 'Female'),
+--(1003, 'Carie', 'Alia', 34, 'Male'),
+--(1004, 'John', 'Kely', 24, 'Male'),
+--(1005, 'Marina', 'Annya', 32, 'Female'),
+--(1006, 'Ford','Dadale', 35, 'Male'),
+--(1007, 'Eaera','Aska', 36, 'Female'),
+--(1009, 'Barbara', 'Olima', 40, 'Female')
+
+INSERT INTO EmployeeSalary VALUES
+(1002, 'Receptionist', 36000),
+(1003, 'Salesman', 63000),
+(1004, 'Accountant', 47000),
+(1005, 'HR', 50000),
+(1006, 'Regional Manager',65000),
+(1007, 'Supplier Relations', 41000),
+(1008, 'Salesman', 48000),
+(1009, 'Accountant', 42000)
---
+++ SQLQuery2.sql
@@ -0,0 +1,7 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [EmployeedID]
+      ,[FirstName]
+      ,[LastName]
+      ,[Age]
+      ,[Gender]
+  FROM [SQL Tutorial].[dbo].[EmployeeDemograpihcs]
