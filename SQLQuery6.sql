f2ca12596f0cc825b168cf67c4e2749aaf22e3a4
Laziz Rahmat <lazizrakhmat@gmail.com>
2023-01-06 09:58:39 +0500

    And, Or, Like, Null, Not Null, In

---
+++ SQLQuery6.sql
@@ -0,0 +1,35 @@
+/*
+Where Statement
+=, <>, <, >, And, Or, Like, Null, Not Null, In
+*/
+
+--SELECT *
+--FROM EmployeeDemograpihcs
+--WHERE FirstName <> 'Jim'
+
+--SELECT *
+--FROM EmployeeDemograpihcs
+--WHERE Age <= 32
+
+--SELECT *
+--FROM EmployeeDemograpihcs
+--WHERE Age <= 35 AND Gender = 'Male'
+
+--SELECT *
+--FROM EmployeeDemograpihcs
+--WHERE Age >=30 OR Gender = 'Female'
+
+--SELECT *
+--FROM EmployeeDemograpihcs
+--WHERE LastName Like 'H%b%'
+
+--SELECT *
+--FROM EmployeeDemograpihcs
+--WHERE FirstName IS NOT NULL 
+
+--SELECT *
+--FROM EmployeeDemograpihcs
+--WHERE FirstName IN ('Jim', 'Halpert')
+
+
+ 
