100d1d6cd8c09a428f885c0854c18842c35a86d6
Laziz Rahmat <lazizrakhmat@gmail.com>
2023-01-06 09:35:01 +0500

    Top, Distinct, Count, As, Max, Min, Avg

---
+++ SQLQuery5.sql
@@ -0,0 +1,32 @@
+/*
+SELECT Statement
+*, Top, Distinct, Count, As, Max, Min, Avg
+*/
+
+--SELECT MAX(Salary)
+--FROM EmployeeSalary
+
+--SELECT TOP 5* 
+--FROM EmployeeDemograpihcs
+
+--SELECT DISTINCT(Salary)
+--FROM EmployeeSalary
+
+--SELECT COUNT(FirstName)
+--FROM EmployeeDemograpihcs
+
+--SELECT MAX(Salary)
+--FROM EmployeeSalary
+
+--SELECT MIN(Age)
+--From EmployeeDemograpihcs
+
+--SELECT AVG(Salary)
+--From EmployeeSalary
+
+--SELECT *
+--FROM EmployeeDemograpihcs
+
+--SELECT *
+--FROM [SQL Tutorial].dbo.EmployeeSalary
+
