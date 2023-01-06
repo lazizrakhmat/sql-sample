b6badce39875c71550ec35963f60a41fb1809409
Laziz Rahmat <lazizrakhmat@gmail.com>
2023-01-06 10:19:13 +0500

    Group By, Order By, DESC

---
+++ SQLQuery7.sql
@@ -0,0 +1,20 @@
+/* 
+Group By, Order By
+*/
+
+--SELECT *
+--FROM EmployeeDemograpihcs
+--ORDER BY Age DESC, Gender DESC
+
+--SELECT Gender, Age, COUNT(Gender) AS CountGender
+--FROM EmployeeDemograpihcs
+--WHERE Age >= 32
+--GROUP BY Gender, Age
+--ORDER BY CountGender DESC
+
+SELECT * 
+FROM EmployeeDemograpihcs
+
+SELECT *
+FROM EmployeeDemograpihcs
+ORDER BY 4 DESC, 5 DESC
